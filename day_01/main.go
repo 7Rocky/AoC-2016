49229d2c18f9d508e2fd48e2cbdf19aa8014ce6b
Rocky <rockygitt@gmail.com>
2021-01-10 16:47:09 +0100

    AoC Problem 01

---
+++ day_01/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func containsPosition(position complex128, reachedPositions []complex128) bool {
+	for _, p := range reachedPositions {
+		if p == position {
+			return true
+		}
+	}
+
+	return false
+}
+
+func getBlocks(position complex128) int {
+	return int(math.Abs(real(position)) + math.Abs(imag(position)))
+}
+
+func main() {
+	file, _ := os.Open("input.txt")
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	scanner.Scan()
+
+	instructions := strings.Split(scanner.Text(), ", ")
+
+	position := 0 + 0i
+	direction := 1i
+
+	twiceReachedPosition := 0 + 0i
+	var reachedPositions []complex128
+
+	for _, instruction := range instructions {
+		if instruction[0] == 'R' {
+			direction *= -1i
+		} else if instruction[0] == 'L' {
+			direction *= 1i
+		}
+
+		steps, _ := strconv.Atoi(instruction[1:])
+
+		for i := 0; i < steps; i++ {
+			if containsPosition(position, reachedPositions) && twiceReachedPosition == 0+0i {
+				twiceReachedPosition = position
+			}
+
+			reachedPositions = append(reachedPositions, position)
+			position += direction
+		}
+	}
+
+	fmt.Printf("Blocks (1): %d\n", getBlocks(position))
+	fmt.Printf("Blocks to twice reached position (2): %d\n", getBlocks(twiceReachedPosition))
+}
