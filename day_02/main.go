eb3164cdd3b445f9d9f1fd6f0b4dd5cb94963d2a
Rocky <rockygitt@gmail.com>
2021-01-10 23:49:58 +0100

    AoC Problem 02

---
+++ day_02/main.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func getBathroomCode(buttons map[complex128]string, instructions []string, position complex128) string {
+	var code string
+
+	for _, instruction := range instructions {
+		for _, d := range instruction {
+			switch d {
+			case 'U':
+				if _, ok := buttons[position+1i]; ok {
+					position += 1i
+				}
+			case 'R':
+				if _, ok := buttons[position+1]; ok {
+					position++
+				}
+			case 'D':
+				if _, ok := buttons[position-1i]; ok {
+					position -= 1i
+				}
+			case 'L':
+				if _, ok := buttons[position-1]; ok {
+					position--
+				}
+			}
+		}
+
+		code += buttons[position]
+	}
+
+	return code
+}
+
+func main() {
+	file, _ := os.Open("input.txt")
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	var instructions []string
+
+	for scanner.Scan() {
+		instructions = append(instructions, scanner.Text())
+	}
+
+	buttons := map[complex128]string{
+		-1 + 1i: "1",
+		1i:      "2",
+		1 + 1i:  "3",
+		-1:      "4",
+		0:       "5",
+		1:       "6",
+		-1 - 1i: "7",
+		-1i:     "8",
+		1 - 1i:  "9",
+	}
+
+	fmt.Printf("Bathroom code (1): %s\n", getBathroomCode(buttons, instructions, 0))
+
+	buttons = map[complex128]string{
+		2i:      "1",
+		-1 + 1i: "2",
+		1i:      "3",
+		1 + 1i:  "4",
+		-2:      "5",
+		-1:      "6",
+		0:       "7",
+		1:       "8",
+		2:       "9",
+		-1 - 1i: "A",
+		-1i:     "B",
+		1 - 1i:  "C",
+		-2i:     "D",
+	}
+
+	fmt.Printf("Bathroom code (2): %s\n", getBathroomCode(buttons, instructions, -2))
+}
