ff942ff65dde4bea64093eb4ee12c3569fd65dee
Rocky <rockygitt@gmail.com>
2021-01-11 22:23:27 +0100

    AoC Problem 05

---
+++ day_05/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"bytes"
+	"crypto/md5"
+	"encoding/hex"
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	input := "cxdnnyjw"
+
+	password1 := ""
+	password2 := bytes.Repeat([]byte{'_'}, 8)
+
+	index := 0
+
+	for len(password1) < 8 || bytes.Contains(password2, []byte{'_'}) {
+		hash := md5.Sum([]byte(input + strconv.Itoa(index)))
+		hashString := hex.EncodeToString(hash[:])
+
+		if strings.HasPrefix(hashString, "00000") {
+			if len(password1) < 8 {
+				password1 += string(hashString[5])
+			}
+
+			position, err := strconv.Atoi(string(hashString[5]))
+
+			if err == nil && position < 8 && password2[position] == '_' {
+				password2[position] = hashString[6]
+			}
+		}
+
+		index++
+	}
+
+	fmt.Printf("Password (1): %s\n", password1)
+	fmt.Printf("Password (2): %s\n", string(password2))
+}
