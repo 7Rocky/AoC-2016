6b4b0eb8d49988b24777666dcdca738f96733db0
Rocky <rockygitt@gmail.com>
2021-01-13 18:39:01 +0100

    Tests

---
+++ day_06/main_test.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+)
+
+func TestMain(t *testing.T) {
+	rescueStdout := os.Stdout
+	r, w, _ := os.Pipe()
+	os.Stdout = w
+
+	main()
+
+	w.Close()
+	out, _ := ioutil.ReadAll(r)
+	os.Stdout = rescueStdout
+
+	want := "Error-corrected message (1): kjxfwkdh\n" +
+		"Error-corrected message (2): xrwcsnps\n"
+
+	if string(out) != want {
+		t.Errorf("\nWant:\n%s\nGot:\n%s", want, out)
+	}
+}
