b103db6aac2f375bfe9b5c6d4f4024d0d5e2ed34
Rocky <rockygitt@gmail.com>
2021-01-15 01:36:01 +0100

    Tests

---
+++ day_09/main_test.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+)
+
+func TestMain(t *testing.T) {
+	rescueStdout := os.Stdout
+	r, w, _ := os.Pipe()
+	os.Stdout = w
+
+	main()
+
+	w.Close()
+	out, _ := ioutil.ReadAll(r)
+	os.Stdout = rescueStdout
+
+	want := "Length of decompressed file V1 (1): 110346\n" +
+		"Length of decompressed file V2 (2): 10774309173\n"
+
+	if string(out) != want {
+		t.Errorf("\nWant:\n%s\nGot:\n%s", want, out)
+	}
+}
